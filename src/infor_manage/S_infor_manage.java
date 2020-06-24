35b494aa4e560678fb6b8c958ba0c37054b4c50d
Victorbvd <51233781+Victorbvd@users.noreply.github.com>
2020-06-25 01:27:15 +0800

    Update S_infor_manage.java

--- src/infor_manage/S_infor_manage.java
+++ src/infor_manage/S_infor_manage.java
@@ -1,5 +1,4 @@
 package infor_manage;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,20 +6,17 @@ import java.awt.event.ActionListener;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-import M_and_S.Manager;
+import System.Manager;
+import ButtonFrame.*;
 
 public class S_infor_manage {
     private JFrame frame = new JFrame();
     private JPanel jpl = new JPanel(); 
     private JButton button1 = new JButton("查询学生信息");
     private JButton button2 = new JButton("新增学生信息");
-    private JButton button3 = new JButton("更改学生信息");
-    private JButton button4 = new JButton("删除学生信息");
-    private JButton button5 = new JButton("返回");
+    private JButton button3 = new JButton("删除学生信息");
+    private JButton button4 = new JButton("返回");
     public S_infor_manage() {
         frame.setTitle("学生信息管理");
         frame.setSize(500, 350);
@@ -29,30 +25,25 @@ public class S_infor_manage {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        button1.setBounds(50, 50, 175, 50);
+        button1.setBounds(50, 75, 175, 50);
         button1.setFont(new Font("宋体", Font.BOLD, 20));
         jpl.add(button1);
-        button1.addActionListener(new ButtonListener1());
+        button1.addActionListener(new SearchListener());
 
-        button2.setBounds(275, 50, 175, 50);
+        button2.setBounds(275, 75, 175, 50);
         button2.setFont(new Font("宋体", Font.BOLD, 20));
         jpl.add(button2);
-        button2.addActionListener(new ButtonListener2());
+        button2.addActionListener(new AddListener());
 
-        button3.setBounds(50, 150, 175, 50);
+        button3.setBounds(50, 175, 175, 50);
         button3.setFont(new Font("宋体", Font.BOLD, 20));
         jpl.add(button3);
-        button3.addActionListener(new ButtonListener3());
+        button3.addActionListener(new DelListener());
         
-        button4.setBounds(275, 150, 175, 50);
+        button4.setBounds(275, 175, 175, 50);
         button4.setFont(new Font("宋体", Font.BOLD, 20));
         jpl.add(button4);
-        button4.addActionListener(new ButtonListener4());
-        
-        button5.setBounds(162, 250, 175, 50);
-        button5.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl.add(button5);
-        button5.addActionListener(new ButtonListener5());
+        button4.addActionListener(new BackListener());      
         
         ButtonGroup buttonGroup = new ButtonGroup();
         buttonGroup.add(button1);
@@ -63,211 +54,40 @@ public class S_infor_manage {
         jpl.setLayout(null);
         frame.add(jpl);
 
-        
-
     }
-    private class ButtonListener1 implements ActionListener {
+    private class SearchListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             frame.dispose();
-            new search();
+            new StudentSearch();
         }
-            
     }
 
-    private class ButtonListener2 implements ActionListener {
+    private class AddListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             frame.dispose();
-            new add();
+            try {
+				new StudentAdd();
+			} catch (Exception e1) {
+				// TODO 自动生成的 catch 块
+				e1.printStackTrace();
+			}
         }
-            
     }
 
-    private class ButtonListener3 implements ActionListener {
+    private class DelListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             frame.dispose();
-            new change();
+            new StudentDel();
         }
-            
     }
-    private class ButtonListener4 implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            frame.dispose();
-            new del();
-        }
-            
-    }
-    private class ButtonListener5 implements ActionListener {
+    private class BackListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             frame.dispose();
             new Manager();
-        }
-            
-    }
-    class search{
-    	private JFrame frame = new JFrame();
-        public JTextField ID = new JTextField();
-        private JPanel jpl1 = new JPanel();    
-        private JButton button1 = new JButton("确定");
-        private JButton button2 = new JButton("返回");
-    	public search() {
-        frame.setTitle("查询学生信息");
-        frame.setSize(500, 350);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        JLabel lab1 = new JLabel("请输入学生学号：");
-        lab1.setBounds(25, 20, 225, 50);
-        lab1.setFont(new Font("宋体", Font.BOLD, 25));
-        jpl1.add(lab1);
-        
-        ID.setBounds(250, 26, 225, 40);
-        ID.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl1.add(ID);
-        
-        button1.setBounds(100, 75, 100, 50);
-        button1.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl1.add(button1);
-        
-        button2.setBounds(300, 75, 100, 50);
-        button2.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl1.add(button2);
-        button2.addActionListener(new ButtonListener12());
-        
-        jpl1.setLayout(null);
-        frame.add(jpl1);
-    	}
-    	
-        private class ButtonListener12 implements ActionListener {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                new S_infor_manage();
-            }
-                
-        }
-    }
-    
-    class add{
-    	private JFrame frame = new JFrame();
-        private JPanel jpl2 = new JPanel();    
-        private JButton button1 = new JButton("确定");
-        private JButton button2 = new JButton("返回");
-    	public add() {
-        frame.setTitle("新增学生信息");
-        frame.setSize(500, 350);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        
-        
-        button1.setBounds(100, 250, 100, 50);
-        button1.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl2.add(button1);
-        
-        button2.setBounds(300, 250, 100, 50);
-        button2.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl2.add(button2);
-        button2.addActionListener(new ButtonListener22());
-        
-        jpl2.setLayout(null);
-        frame.add(jpl2);
-    	}
-    	
-        private class ButtonListener22 implements ActionListener {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                new S_infor_manage();
-            }
-                
-        }
-    }
-    
-    class change{
-    	private JFrame frame = new JFrame();
-        private JPanel jpl3 = new JPanel();    
-        private JButton button1 = new JButton("确定");
-        private JButton button2 = new JButton("返回");
-    	public change() {
-        frame.setTitle("更改学生信息");
-        frame.setSize(500, 350);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        
-        button1.setBounds(100, 250, 100, 50);
-        button1.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl3.add(button1);
-        
-        button2.setBounds(300, 250, 100, 50);
-        button2.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl3.add(button2);
-        button2.addActionListener(new ButtonListener32());
-        
-        jpl3.setLayout(null);
-        frame.add(jpl3);
-    	}
-    	
-        private class ButtonListener32 implements ActionListener {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                new S_infor_manage();
-            }
-                
-        }
-    }
-    
-    class del{
-    	private JFrame frame = new JFrame();
-        public JTextField ID = new JTextField();
-        private JPanel jpl4 = new JPanel();    
-        private JButton button1 = new JButton("确定");
-        private JButton button2 = new JButton("返回");
-    	public del() {
-        frame.setTitle("删除学生信息");
-        frame.setSize(500, 350);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        JLabel lab1 = new JLabel("请输入学生学号：");
-        lab1.setBounds(25, 20, 225, 50);
-        lab1.setFont(new Font("宋体", Font.BOLD, 25));
-        jpl4.add(lab1);
-        
-        ID.setBounds(225, 26, 240, 40);
-        ID.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl4.add(ID);
-        
-        button1.setBounds(100, 75, 100, 50);
-        button1.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl4.add(button1);
-        
-        button2.setBounds(300, 75, 100, 50);
-        button2.setFont(new Font("宋体", Font.BOLD, 20));
-        jpl4.add(button2);
-        button2.addActionListener(new ButtonListener42());
-        
-        jpl4.setLayout(null);
-        frame.add(jpl4);
-    	}
-    	
-        private class ButtonListener42 implements ActionListener {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                new S_infor_manage();
-            }
-                
-        }
+        }    
     }
 }
