728b5e32074d4fabd234dc98fd623fdeb60950ce
ALEX6467 <52844799+ALEX6467@users.noreply.github.com>
2020-07-01 14:35:21 +0800

    Add files via upload

---
+++ src/Model/ManagersModel.java
@@ -0,0 +1,35 @@
+package Model;
+
+public class StudentsModel {
+
+	private String name,sex,contact,student_id;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getManager_id() {
+		return manager_id;
+	}
+	public void setManager_id(String manager_id) {
+		this.manager_id = manager_id;
+	}
+
+
+
+}
